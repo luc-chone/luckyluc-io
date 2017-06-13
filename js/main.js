800bfd8097abb93b3f051dbf35198d33e91e9b7d
Luc Chone <luc.chone@gmail.com>
2017-06-12 20:07:18 -0700

    . initial

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var gameProc = function(processingInstance) {
+
+	with(processingInstance){
+		size(1000,1000);
+
+		noStroke();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
+		};
