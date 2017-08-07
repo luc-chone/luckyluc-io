b4c47d8b6da0c9d8b3b6fc036982a3801776c292
Luc Chone <luc.chone@gmail.com>
2017-08-07 14:49:00 -0700

    Moving plane and car working. Same left and right limit.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,65 @@
 
-document.addEventListener("DOMContentLoaded", function(event) {
-	// move the plane
-	var plane1El = document.getElementById("plane-1");
-	plane1El.style.left = "400px";
+var plane1El;
+var car1El;
 
-	// draw the building
-	var building1Canvas = document.getElementById("building-1");	
-	rect(building1Canvas, 10, 10, 60, 50, "red", "blue");
-  
-  var building2Canvas = document.getElementById("building-2");	
-	rect(building2Canvas, 10, 10, 60, 100, "gray", "black");
+var limitLeft = 50;
+var limitRight = 800;
+var direction = 1; 
+var step = 5;
 
-  var building3Canvas = document.getElementById("building-3");	
-	rect(building3Canvas, 10, 10, 60, 100, "gray", "black");
+// ------- P5 app code ------- //
+function setup(){
 
- var building4Canvas  = document.getElementById("building-4");	
-	rect(building4Canvas, 10, 10, 60, 100, "gray", "black");
-
-});
-
-
-
-function rect(canvas, x, y, w, h, fill, stroke){
-	var context = canvas.getContext('2d');
-  context.beginPath();
-  context.rect(x, y, w, h);
-  context.fillStyle = fill;
-  context.fill();
-  context.lineWidth = 2;
-  context.strokeStyle = stroke;
-  context.stroke();	
 }
+
+function draw(){
+	
+	// ------ Moving Plane ------- //
+	// get the current left
+	var currentLeft = plane1El.offsetLeft;
+
+	// decide the direction
+	if (currentLeft > limitRight){
+		plane1El.classList.add("left");
+		direction = -1;
+	}else if (currentLeft < limitLeft){
+		plane1El.classList.remove("left");
+		direction = 1;
+	}
+
+	// get the new left position from the direction
+	var newLeft = currentLeft + direction * step;
+
+	plane1El.style.left = newLeft + "px";
+	// ------ /Moving Plane ------- //
+
+	// ------ Moving Car ------- //
+	// get the current left
+	var currentLeft = car1El.offsetLeft;
+
+	// decide the direction
+	if (currentLeft > limitRight){
+		car1El.classList.add("left");
+		direction = -1;
+	}else if (currentLeft < limitLeft){
+		car1El.classList.remove("left");
+		direction = 1;
+	}
+
+	// get the new left position from the direction
+	var newLeft = currentLeft + direction * step;
+
+	car1El.style.left = newLeft + "px";
+	// ------ /Moving Car ------- //	
+};
+
+// ------- /P5 app code ------- //
+
+// This will be called when the page is loaded (before setup and draw)
+document.addEventListener("DOMContentLoaded", function(event) {
+	// assign plane1El to point to the DOM plane-1 element.
+	plane1El = document.getElementById("plane-1");
+	// assign car1El
+	car1El = document.getElementById("car-1");
+});
 
